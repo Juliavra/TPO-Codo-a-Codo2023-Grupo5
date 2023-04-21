da27699d56608308e8671ac8f30161be63b3c830
Julian Avramides <julianavramides@hotmail.com>
2023-04-21 01:02:23 -0300

    Form Listo, Sin Validacion JS

---
+++ index.js
@@ -0,0 +1,25 @@
+//console.log("Hola Mundo");
+//document.write("Hola write");
+
+/*console.log(nombre);*/
+/*document.write(nombre);*/
+/*
+    function iniciar()
+    {
+    var boton = document
+    .getElementById("Enviar");
+    boton.addEventListener("click", enviarformulario); 
+    }
+    function enviarformulario()
+    {
+    var lista = document.forms;
+    var formulario = lista[0];
+    console.log(formulario);
+    formulario.submit(); 
+    }
+    window.addEventListener("load", iniciar);
+*/
+
+var lista = document.forms;
+var formulario = lista[0];
+console.log(formulario);
